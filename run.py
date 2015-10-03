748b9f6bbdb974d6e1ffe53a2659dd08c68619e9
Alan Liu <alanl.liu@yahoo.com>
2015-10-03 14:18:11 -0400

    prize

--- run.py
+++ run.py
@@ -9,5 +9,9 @@ def home():
 def ian():
 	return render_template('ian.html')
 
+@app.route('/1203071595')
+def prize():
+	return "CONGRATZ!!! You did it!!!! Meet us in front of Sterling!"
+
 if __name__ == '__main__':
     app.run(debug = True)

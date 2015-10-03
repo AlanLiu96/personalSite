10a2098c7f60a92de517eaacb828c1256eb22194
Alan Liu <alanl.liu@yahoo.com>
2015-10-03 14:03:49 -0400

    ian v1

--- run.py
+++ run.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/ian')
+def ian():
+	return render_template('ian.html')
+
 if __name__ == '__main__':
     app.run(debug = True)

609def738e14ff272d3bff03317d46b5459ca25f
Alan Liu <alanl.liu@yahoo.com>
2015-10-06 23:05:29 -0400

    remove ian's stuff

--- run.py
+++ run.py
@@ -5,13 +5,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/ian')
-def ian():
-	return render_template('ian.html')
-
-@app.route('/1203071595')
-def prize():
-	return "CONGRATZ!!! You did it!!!! Meet us in front of Sterling!"
-
 if __name__ == '__main__':
     app.run(debug = True)

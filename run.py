17292ed9f4aa1f2d938e6877952f91159275de7b
Alan Liu <AlanLiu96@users.noreply.github.com>
2022-12-29 23:18:06 -0800

    privacy policy at alanliu.dev/privacy_policy

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,10 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/work')
 def work():
     return render_template('work.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/privacy_policy')
+def privacy_policy():
+    return render_template('privacy.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=('DEBUG' in os.environ), host='0.0.0.0', port=7000)
